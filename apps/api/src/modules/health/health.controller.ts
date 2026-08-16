23155af53250327318092b6162693d85d1559150
victorgabrieldeon <victordeonroyale@gmail.com>
2026-08-16 13:04:55 -0300

    feat(health): Adiciona módulo de Health

---
+++ apps/api/src/modules/health/health.controller.ts
@@ -0,0 +1,19 @@
+import { TypedRoute } from '@nestia/core';
+import { Controller } from '@nestjs/common';
+
+interface HealthResponse {
+  /**
+   * Estado atual do serviço.
+   *
+   * @title Estado do serviço
+   */
+  status: 'ok';
+}
+
+@Controller()
+export class HealthController {
+  @TypedRoute.Get('health')
+  health(): HealthResponse {
+    return { status: 'ok' };
+  }
+}
---
+++ apps/api/src/modules/health/health.module.ts
@@ -0,0 +1,6 @@
+import { Module } from '@nestjs/common';
+
+import { HealthController } from './health.controller.js';
+
+@Module({ controllers: [HealthController] })
+export class HealthModule {}
